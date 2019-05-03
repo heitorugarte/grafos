0ea9e5c596c8a18cf7ab18ade807848bb01e7849
Heitor Ugarte Calvet da Silveira <38788613+heitorugarte@users.noreply.github.com>
2019-05-02 21:02:08 -0300

    Leitura de arquivo de texto
    Leitura de arquivo de texto para montar um grafo.
    
    O arquivo de texto é lido linha a linha, contabilizando inicialmente o número de vértices e após isso as adjacências serão criadas.
    Atualmente, o código só imprime as adjacências entre os vértices encontrados.

---
+++ POC.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	FILE *arquivo;
+	char str[20];
+	char* nomearquivo = "c:\\teste1.txt";
+	
+	arquivo = fopen(nomearquivo, "r");
+	if (arquivo == NULL){
+		printf("Nao foi possivel abrir o arquivo %s", nomearquivo);	
+		return 1;
+	}
+	
+	// ler a primeira linha colocando em um int
+	fgets(str, 20, arquivo);
+	int qntVertices = atoi(str);
+	printf("Quantidade de vertices: %d\n", qntVertices);
+	
+	//Ler adjacencias
+	while (fgets(str, 20, arquivo) != NULL){
+		char* split = strtok(str, " ");
+		char* pares[2];
+		int i = 0;
+		while (split != NULL){
+			pares[i++] = split;
+			split = strtok(NULL, " ");
+	 	}
+		int origem = atoi(pares[0]);
+		int destino = atoi(pares[1]);
+		printf("Adjacencia entre: %d e %d\n", origem, destino);
+		// add adjacencia (int 1, int 2)
+	}
+	fclose(arquivo);
+	
+	// bfs()
+	// exibir saida no formato do trabalho
+	return 0;
+}
